65df35b1af13e1630c0c9cc7229ad05d0e7bc883
Fabio_Araujow <araujobimmer@gmail.com>
2026-05-21 11:41:05 +0100

    final version

--- ft_printf.c
+++ ft_printf.c
@@ -12,13 +12,38 @@
 
 #include "ft_printf.h"
 
-int	ft_check_dispatcher(const char *str, int i, va_list ap)
+int	ft_check_dispatcher(char spec, va_list ap)
 {
-	int	pos;
-
-	pos = i;
-	if (str[i + 1] == 'c')
+	if (spec == 'c')
 		return (ft_putchar_pf(va_arg(ap, int)));
+	if (spec == 's')
+		return (ft_putstr_pf(va_arg(ap, char *)));
+	if (spec == 'p')
+		return (ft_putptr_pf(va_arg(ap, void *)));
+	if (spec == 'd' || spec == 'i')
+		return (ft_putnbr_pf(va_arg(ap, int)));
+	if (spec == 'u')
+		return (ft_putunsigned_pf(va_arg(ap, unsigned int)));
+	if (spec == 'x' || spec == 'X')
+		return (ft_puthex_pf(va_arg(ap, unsigned int), spec));
+	return (ft_putchar_pf(spec));
+}
+
+static int	ft_process(const char *format, int *i, va_list ap)
+{
+	int	ret;
+
+	if (format[*i] == '%')
+	{
+		ret = ft_check_dispatcher(format[*i + 1], ap);
+		*i += 2;
+	}
+	else
+	{
+		ret = ft_putchar_pf(format[*i]);
+		(*i)++;
+	}
+	return (ret);
 }
 
 int	ft_printf(const char *format, ...)
@@ -26,20 +51,17 @@ int	ft_printf(const char *format, ...)
 	va_list	ap;
 	int		i;
 	int		count;
+	int		ret;
 
 	i = 0;
 	count = 0;
 	va_start(ap, format);
 	while (format[i])
 	{
-		if (format[i] == '%')
-		{
-			count = count + ft_check_dispatcher(format, i, ap);
-			i++;
-		}
-		else
-			ft_putchar_pf(format[i]);
-		i++;
+		ret = ft_process(format, &i, ap);
+		if (ret == -1)
+			return (va_end(ap), -1);
+		count += ret;
 	}
 	va_end(ap);
 	return (count);
